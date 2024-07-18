1fc0e84384b54fed2f92bfc09ff5f13770db8ab8
AlexG <Alexanderg.savitr@gmail.com>
2024-07-18 15:53:27 -0500

    Added pool of words

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 console.log('test');
+
+/*-------------------------------- Constants --------------------------------*/
+const wordPool = ['theory', 'outcome','desk','meal','arrival','birthday','weakness',
+    'currency','volume','income','science','connection','description','love',
+    'media','consequence','pizza','cigarette','blood','dirt','trainer','relationship',
+    'beer','knowledge','improvement','resolution','payment','customer','charity',
+    'failure','initiative','bonus','permision','platform','judgment','requirement',
+    'chest','personality','coffee','wood','garbage','response','imagination',
+    'marriage','excitement','resource','cheek','pupil','committee','mode']
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let winningWords =[]
+let assassinWords = []
+let bystanderWords = []
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
